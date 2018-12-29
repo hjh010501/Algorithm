ca46ad432d110181615eb505767776cb5854e3e8
kidevelop <hjh010501@naver.com>
2018-12-29 22:51:58 +0900

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by KID on 29/12/2018.
+//  Copyright © 2018 kidevelop. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
